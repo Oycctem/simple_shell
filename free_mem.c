950f1aa95adbe42baa2fe215587ac6cf9cf1fc9f
anouar05 <anouar.haimouda.2000@gmail.com>
2023-10-18 18:23:15 -0700

    Shell 2

--- free_mem.c
+++ free_mem.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 /**
- * free_mem - frees the memory assosciated with an array of tokens
- * @tokens: the array of tokens to be freed
- * @num_tokens: a pointer to the number of tokens in the array
+ * free_mem - frees the memory assosciated with an array of tokens.
+ * @tokens: the array of tokens to be freed.
+ * @num_tokens: a pointer to the number of tokens in the array.
  */
 void free_mem(char **token, int *num_tokens)
 {
