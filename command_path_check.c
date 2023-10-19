61357c0b1758040095a6c066630c5de3606f2738
anouar05 <anouar.haimouda.2000@gmail.com>
2023-10-18 18:19:58 -0700

    Shell

--- command_path_check.c
+++ command_path_check.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 /**
- * command_path_check - check if a command is specified with a path
- * @cmd: the command
+ * command_path_check - check if a command is specified with a path.
+ * @cmd: the command.
  * Return: returns 1 if the command is specified with a path else 0
  */
 int command_path_check(const char *cmd)
